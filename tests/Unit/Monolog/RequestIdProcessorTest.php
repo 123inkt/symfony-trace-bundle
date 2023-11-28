635627e8dc7cdfc4880a7f48a1fb6d1a83c3cc67
Frank Dekker <fdekker@123inkt.nl>
2023-11-28 22:26:06 +0100

    Fix phpunit logger test

--- tests/Unit/Monolog/RequestIdProcessorTest.php
+++ tests/Unit/Monolog/RequestIdProcessorTest.php
@@ -7,6 +7,7 @@ use DateTimeImmutable;
 use DR\SymfonyRequestId\Monolog\RequestIdProcessor;
 use DR\SymfonyRequestId\RequestIdStorage;
 use Monolog\Level;
+use Monolog\Logger;
 use Monolog\LogRecord;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -26,6 +27,10 @@ class RequestIdProcessorTest extends TestCase
 
     public function testProcessorDoesNotSetRequestIdWhenNoIdIsPresent(): void
     {
+        if (version_compare((string)Logger::API, '3', 'lt')) {
+            self::markTestSkipped('The Monolog at least 3 is required to run this test.');
+        }
+
         $this->idStorage->expects(static::once())->method('getRequestId')->willReturn(null);
 
         $record = ($this->processor)(new LogRecord(new DateTimeImmutable('now'), 'channel', Level::Info, 'foo'));
@@ -35,6 +40,10 @@ class RequestIdProcessorTest extends TestCase
 
     public function testProcessorAddsRequestIdWhenIdIsPresent(): void
     {
+        if (version_compare((string)Logger::API, '3', 'lt')) {
+            self::markTestSkipped('The Monolog at least 3 is required to run this test.');
+        }
+
         $this->idStorage->expects(static::once())->method('getRequestId')->willReturn('abc123');
 
         $record = ($this->processor)(new LogRecord(new DateTimeImmutable('now'), 'channel', Level::Info, 'foo'));
@@ -45,6 +54,10 @@ class RequestIdProcessorTest extends TestCase
 
     public function testProcessorAddsRequestIdWhenIdIsPresentArrayFormat(): void
     {
+        if (version_compare((string)Logger::API, '3', 'ge')) {
+            self::markTestSkipped('The version 1 or 2 of Monolog is required to run this test.');
+        }
+
         $this->idStorage->expects(static::once())->method('getRequestId')->willReturn('abc123');
 
         $record = ($this->processor)([]);
