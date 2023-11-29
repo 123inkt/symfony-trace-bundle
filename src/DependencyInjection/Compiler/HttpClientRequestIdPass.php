b32f2f09b394400c6978f9db44dd807601bfa98a
Barthold Bos <barthold@123inkt.nl>
2023-11-29 13:11:58 +0100

    Add RequestIdAwareHttpClient and CompilerPass

---
+++ src/DependencyInjection/Compiler/HttpClientRequestIdPass.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\SymfonyRequestId\DependencyInjection\Compiler;
+
+use DR\SymfonyRequestId\Http\RequestIdAwareHttpClient;
+use DR\SymfonyRequestId\RequestIdStorage;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class HttpClientRequestIdPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition('http_client')) {
+            $container->getDefinition('http_client')
+                ->addTag('http_client.request_id');
+        }
+
+        $taggedServices = $container->findTaggedServiceIds('http_client.request_id');
+
+        foreach ($taggedServices as $id => $tag) {
+            $container->register($id . '.request_id', RequestIdAwareHttpClient::class)
+                ->setArguments([new Reference($id . '.request_id' . '.inner'), new Reference(RequestIdStorage::class)])
+                ->setDecoratedService($id, null, 10);
+        }
+    }
+}
---
+++ src/Http/RequestIdAwareHttpClient.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\SymfonyRequestId\Http;
+
+use DR\SymfonyRequestId\RequestIdStorage;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\ResponseStreamInterface;
+use Symfony\Contracts\Service\ResetInterface;
+
+#[AsDecorator('http_client')]
+class RequestIdAwareHttpClient implements HttpClientInterface, ResetInterface, LoggerAwareInterface
+{
+    public function __construct(private HttpClientInterface $client, private readonly RequestIdStorage $storage)
+    {
+    }
+
+    public function request(string $method, string $url, array $options = []): ResponseInterface
+    {
+        $options['headers']['X-Request-Id'] = $this->storage->getRequestId();
+
+        return $this->client->request($method, $url, $options);
+    }
+
+    public function stream(iterable|ResponseInterface $responses, ?float $timeout = null): ResponseStreamInterface
+    {
+        return $this->client->stream($responses, $timeout);
+    }
+
+    public function reset(): void
+    {
+        if ($this->client instanceof ResetInterface) {
+            $this->client->reset();
+        }
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        if ($this->client instanceof LoggerAwareInterface) {
+            $this->client->setLogger($logger);
+        }
+    }
+
+    public function withOptions(array $options): static
+    {
+        $this->client = $this->client->withOptions($options);
+
+        return $this;
+    }
+}
