e94cf8b126fe7af13802efc4bf78da2d8288d6e3
Frank Dekker <fdekker@123inkt.nl>
2023-12-21 11:22:27 +0100

    Add functional test

---
+++ tests/Functional/SentryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\SymfonyTraceBundle\Tests\Functional;
+
+use DR\SymfonyTraceBundle\Tests\Functional\App\Service\MockSentryHub;
+use Exception;
+use PHPUnit\Framework\Attributes\CoversNothing;
+use ReflectionClass;
+use Sentry\State\HubInterface;
+use Sentry\State\Scope;
+
+#[CoversNothing]
+class SentryTest extends AbstractWebTestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testTraceIdIsAddedToSentryHub(): void
+    {
+        $client = self::createClient();
+        $hub    = self::getContainer()->get(HubInterface::class);
+        static::assertInstanceOf(MockSentryHub::class, $hub);
+
+        $client->request('GET', '/', [], [], ['HTTP_TRACE_ID' => 'testId']);
+        static::assertResponseIsSuccessful();
+
+        $scope = $hub->getScope();
+        self::assertScopeHasTag($scope, 'trace_id');
+        self::assertScopeHasTag($scope, 'transaction_id');
+    }
+
+    private static function assertScopeHasTag(Scope $scope, string $key): void
+    {
+        $class    = new ReflectionClass($scope);
+        $property = $class->getProperty('tags');
+        $property->setAccessible(true);
+
+        $tags = $property->getValue($scope);
+        static::assertIsArray($tags);
+        static::assertArrayHasKey($key, $tags);
+    }
+}
